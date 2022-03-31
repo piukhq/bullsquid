c75e0c6d1e87eba41f504169d013e18124ea0ac4
backwardspy <backwardspy@gmail.com>
2022-03-31 18:08:47 +0100

    :sparkles: feat(api): add uniqueness checks to update merchant endpoint

--- bullsquid/merchant_data/api/v1/routers/merchants.py
+++ bullsquid/merchant_data/api/v1/routers/merchants.py
@@ -1,10 +1,10 @@
 """Defines endpoints under the /merchants prefix"""
-from typing import Any, Mapping, Type, TypeAlias
+from typing import Any, Type
+from uuid import UUID
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter
 from piccolo.table import Table
-from pydantic import BaseModel
-from starlette.status import HTTP_204_NO_CONTENT
+from starlette import status
 
 from bullsquid.api.errors import APIMultiError, ResourceNotFoundError, UniqueError
 from bullsquid.merchant_data import db
@@ -12,24 +12,26 @@ from bullsquid.merchant_data.models import Merchant, MerchantWithPK
 
 router = APIRouter(prefix="/merchants")
 
-ViewResponse: TypeAlias = (
-    Response | Mapping[str, Any] | list[Mapping[str, Any]] | BaseModel | list[BaseModel]
-)
 
-
-async def field_is_unique(model: Type[Table], field: str, value: Any) -> bool:
+async def field_is_unique(
+    model: Type[Table], field: str, value: Any, *, pk: UUID | None = None
+) -> bool:
     """Returns true if the given field on the given table is unique, false otherwise."""
-    return not await model.exists().where(getattr(model, field) == value)
+    field = getattr(model, field)
+    if pk:
+        pk_field = getattr(model, "pk")
+        return not await model.exists().where(pk_field != pk, field == value)
+    return not await model.exists().where(field == value)
 
 
 @router.get("", response_model=list[MerchantWithPK])
-async def merchants() -> ViewResponse:
+async def merchants() -> list[dict]:
     """List all Merchants."""
     return [m.to_dict() for m in await db.list_merchants()]
 
 
 @router.post("", response_model=MerchantWithPK)
-async def create_merchant(merchant_data: Merchant) -> ViewResponse:
+async def create_merchant(merchant_data: Merchant) -> dict:
     """Create a new Merchant."""
     if errors := [
         UniqueError(loc=("body", field))
@@ -43,8 +45,17 @@ async def create_merchant(merchant_data: Merchant) -> ViewResponse:
 
 
 @router.put("/{merchant_ref}", response_model=MerchantWithPK)
-async def update_merchant(merchant_ref: str, merchant_data: Merchant) -> ViewResponse:
+async def update_merchant(merchant_ref: UUID, merchant_data: Merchant) -> dict:
     """Update a merchant's details."""
+    if errors := [
+        UniqueError(loc=("body", field))
+        for field in ["name", "slug", "plan_id"]
+        if not await field_is_unique(
+            db.Merchant, field, merchant_data.dict()[field], pk=merchant_ref
+        )
+    ]:
+        raise APIMultiError(errors)
+
     try:
         merchant = await db.update_merchant(merchant_ref, merchant_data.dict())
     except db.NoSuchRecord as ex:
@@ -55,8 +66,12 @@ async def update_merchant(merchant_ref: str, merchant_data: Merchant) -> ViewRes
     return merchant.to_dict()
 
 
-@router.delete("/{merchant_ref}")
-async def delete_merchant(merchant_ref: str) -> ViewResponse:
+@router.delete(
+    "/{merchant_ref}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={status.HTTP_404_NOT_FOUND: {}},
+)
+async def delete_merchant(merchant_ref: UUID) -> None:
     """Delete a merchant."""
     try:
         await db.delete_merchant(merchant_ref)
@@ -64,5 +79,3 @@ async def delete_merchant(merchant_ref: str) -> ViewResponse:
         raise ResourceNotFoundError(
             loc=("path", "merchant_ref"), resource_name="Merchant"
         ) from ex
-
-    return Response(status_code=HTTP_204_NO_CONTENT)

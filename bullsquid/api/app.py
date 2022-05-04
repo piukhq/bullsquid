7b1d2c26a4ada16d54a32e7898518ddabe3a3235
backwardspy <backwardspy@gmail.com>
2022-05-04 15:14:29 +0100

    feat(api): :passport_control: add CORS middleware allowing all origins

--- bullsquid/api/app.py
+++ bullsquid/api/app.py
@@ -3,6 +3,7 @@ Defines the create_app function used to initialize the application.
 """
 from asyncpg.exceptions import PostgresError
 from fastapi import Depends, FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from piccolo.engine import engine_finder
 from starlette.responses import JSONResponse
@@ -20,6 +21,13 @@ def create_app() -> FastAPI:
         title="Portal API",
         description="API for interacting with the portal backend.",
         version="1.0.0",
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
     app.include_router(status_api, tags=["Status"])
     app.include_router(

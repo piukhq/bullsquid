a66f0a277b792950ca0cef89da6cbaade3f79b8d
backwardspy <backwardspy@gmail.com>
2022-05-04 16:44:31 +0100

    feat(api): :sparkles: add create merchant endpoint

--- bullsquid/merchant_data/tables.py
+++ bullsquid/merchant_data/tables.py
@@ -33,7 +33,7 @@ class Merchant(Table):
 
     pk = UUID(primary_key=True)
     name = Text(required=True, unique=True)
-    status = Text(choices=PlanStatus)
+    status = Text(choices=PlanStatus, default=PlanStatus.ACTIVE)
     icon_url = Text(null=True, default=None)
     location_label = Text(required=True)
     is_deleted = Boolean(default=False)

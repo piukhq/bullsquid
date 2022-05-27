a01ec0a56dbee986c27f9cf7063421c5d9874f21
Bilal Leadbitter <bleadbitter@bink.com>
2022-05-27 16:50:52 +0100

    feat: :sparkles: add url validator to accept more flexible entries

--- bullsquid/merchant_data/plans/models.py
+++ bullsquid/merchant_data/plans/models.py
@@ -1,17 +1,17 @@
 """Request & response model definitions for plan endpoints."""
 
 
-from pydantic import UUID4, HttpUrl, validator
+from pydantic import UUID4, validator
 
 from bullsquid.merchant_data.models import BaseModel
-from bullsquid.merchant_data.validators import string_must_not_be_blank
+from bullsquid.merchant_data.validators import FlexibleUrl, string_must_not_be_blank
 
 
 class CreatePlanRequest(BaseModel):
     """Request model for creating/replacing a plan."""
 
     name: str
-    icon_url: HttpUrl | None
+    icon_url: FlexibleUrl | None
     slug: str | None
     plan_id: int | None
 
@@ -24,7 +24,7 @@ class PlanMetadataResponse(BaseModel):
     name: str
     plan_id: int | None
     slug: str | None
-    icon_url: HttpUrl | None
+    icon_url: FlexibleUrl | None
 
     _ = validator("name", "slug", allow_reuse=True)(string_must_not_be_blank)
 

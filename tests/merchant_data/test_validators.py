340a6413f1bc5c43039611a718a1911e8c4a15c4
Bilal Leadbitter <bleadbitter@bink.com>
2022-05-27 17:14:21 +0100

    fix: :bug: fix build test conditions

--- tests/merchant_data/test_validators.py
+++ tests/merchant_data/test_validators.py
@@ -12,30 +12,30 @@ class TestFlexibleUrl(BaseModel):
 
 
 @test("URL is formatted correctly without 'https://'")
-def _():
+def _() -> None:
     example_outcome = TestFlexibleUrl(url="www.example.com")
     assert example_outcome.url == "https://www.example.com/"
 
 
 @test("URL is formatted correctly without '.com'")
-def _():
+def _() -> None:
     example_outcome = TestFlexibleUrl(url="https://www.example")
     assert example_outcome.url == "https://www.example/"
 
 
 @test("URL is formatted correctly without 'https://www.'")
-def _():
+def _() -> None:
     example_outcome = TestFlexibleUrl(url="example.com")
     assert example_outcome.url == "https://example.com/"
 
 
 @test("URL is formatted incorrectly")
-def _():
+def _() -> None:
     with raises(ValidationError):
         example_outcome = TestFlexibleUrl(url="example")
 
 
 @test("URL is formatted correctly with icon.png")
-def _():
+def _() -> None:
     example_outcome = TestFlexibleUrl(url="https://www.example.com/icon.png")
     assert example_outcome.url == "https://www.example.com/icon.png"

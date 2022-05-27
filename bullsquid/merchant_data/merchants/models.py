1c8160c9d66c6bf4827af128d24a060027e6f5ad
Bilal Leadbitter <bleadbitter@bink.com>
2022-05-27 16:50:52 +0100

    :sparkles: feat(api): add url validator to accept more flexible entries

--- bullsquid/merchant_data/merchants/models.py
+++ bullsquid/merchant_data/merchants/models.py
@@ -1,15 +1,15 @@
 """Request & response model definitions for merchant endpoints."""
-from pydantic import UUID4, HttpUrl, validator
+from pydantic import UUID4, validator
 
 from bullsquid.merchant_data.models import BaseModel
-from bullsquid.merchant_data.validators import string_must_not_be_blank
+from bullsquid.merchant_data.validators import FlexibleUrl, string_must_not_be_blank
 
 
 class CreateMerchantRequest(BaseModel):
     """Merchant request model."""
 
     name: str
-    icon_url: HttpUrl | None
+    icon_url: FlexibleUrl | None
     location_label: str
 
     _ = validator("name", "location_label", allow_reuse=True)(string_must_not_be_blank)
@@ -19,7 +19,7 @@ class MerchantMetadataResponse(BaseModel):
     """Merchant details response model."""
 
     name: str
-    icon_url: HttpUrl | None
+    icon_url: FlexibleUrl | None
     location_label: str
 
     _ = validator("name", "location_label", allow_reuse=True)(string_must_not_be_blank)
--- bullsquid/merchant_data/validators.py
+++ bullsquid/merchant_data/validators.py
@@ -1,5 +1,7 @@
 """Reusable Pydantic validators."""
 
+from pydantic import HttpUrl
+from url_normalize import url_normalize
 
 def string_must_not_be_blank(value: str | None) -> str | None:
     """
@@ -9,3 +11,14 @@ def string_must_not_be_blank(value: str | None) -> str | None:
         raise ValueError("must not be blank if not null")
 
     return value
+
+
+class FlexibleUrl(HttpUrl):
+    """URL validator for formatting incoming URLs """
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.normalize_url
+        yield from super().__get_validators__()
+    @classmethod
+    def normalize_url(cls, v: str) -> str:
+        return url_normalize(v)

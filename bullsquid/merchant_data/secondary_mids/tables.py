a06530f3e73cae17b191b951b8a9df08fe9eef4c
backwardspy <backwardspy@gmail.com>
2022-05-31 18:12:53 +0100

    feat(db): :card_file_box: add secondary mid table

--- bullsquid/merchant_data/secondary_mids/tables.py
+++ bullsquid/merchant_data/secondary_mids/tables.py
@@ -1,5 +1,5 @@
 # pylint: disable=duplicate-code
-"""Primary MID table definitions."""
+"""Secondary MID table definitions."""
 from piccolo.columns import UUID, ForeignKey, Text, Timestamptz
 from piccolo.table import Table
 
@@ -8,17 +8,16 @@ from bullsquid.merchant_data.enums import (
     ResourceStatus,
     TXMStatus,
 )
-from bullsquid.merchant_data.locations.tables import Location
 from bullsquid.merchant_data.merchants.tables import Merchant
 from bullsquid.merchant_data.payment_schemes.tables import PaymentScheme
 
 
-class PrimaryMID(Table):
-    """Represents a primary MID value."""
+class SecondaryMID(Table):
+    """Represents a secondary MID value."""
 
     pk = UUID(primary_key=True)
-    mid = Text(unique=True)
-    visa_bin = Text(null=True, default=None)
+    secondary_mid = Text(unique=True, required=True)
+    store_name = Text(null=True, default=None)
     payment_scheme = ForeignKey(PaymentScheme, required=True)
     date_added = Timestamptz()
     payment_enrolment_status = Text(
@@ -27,4 +26,3 @@ class PrimaryMID(Table):
     txm_status = Text(choices=TXMStatus, default=TXMStatus.NOT_ONBOARDED)
     status = Text(choices=ResourceStatus, default=ResourceStatus.ACTIVE, index=True)
     merchant = ForeignKey(Merchant, required=True)
-    location = ForeignKey(Location, null=True, default=None)

6a5aa3c53fd92f5f32c3102eba99bcef83b64985
backwardspy <backwardspy@gmail.com>
2022-06-01 15:28:05 +0100

    feat(api): :sparkles: add list merchants endpoint

--- tests/merchant_data/merchants/test_db.py
+++ tests/merchant_data/merchants/test_db.py
@@ -19,103 +19,101 @@ from tests.fixtures import database
 
 
 @test("can get a merchant by primary key")
-async def _(
-    _db: None = database,
-    merchant: Merchant = merchant,
-) -> None:
+async def _(merchant: Merchant = merchant) -> None:
     actual = await get_merchant(merchant.pk, plan_ref=merchant.plan)
     assert actual.pk == merchant.pk
 
 
 @test("getting a merchant that doesn't exist raises NoSuchRecord")
-async def _(_db: None = database, plan: Plan = plan) -> None:
+async def _(plan: Plan = plan) -> None:
     with raises(NoSuchRecord):
         await get_merchant(uuid4(), plan_ref=plan.pk)
 
 
 @test("getting a merchant from a plan that doesn't exist raises NoSuchRecord")
-async def _(_db: None = database, merchant: Merchant = merchant) -> None:
+async def _(merchant: Merchant = merchant) -> None:
     with raises(NoSuchRecord):
         await get_merchant(merchant.pk, plan_ref=uuid4())
 
 
 @test("getting a merchant from a plan it doesn't belong to raises NoSuchRecord")
-async def _(
-    _db: None = database, plan: Plan = plan, merchant: Merchant = merchant
-) -> None:
+async def _(plan: Plan = plan, merchant: Merchant = merchant) -> None:
     with raises(NoSuchRecord):
         await get_merchant(merchant.pk, plan_ref=plan.pk)
 
 
 @test("can list all merchants in an empty database")
-async def _(_db: None = database) -> None:
-    merchants = await list_merchants()
+async def _(plan: Plan = plan) -> None:
+    merchants = await list_merchants(plan.pk)
     assert len(merchants) == 0
 
 
 @test("can list all merchants in a populated database")
-async def _(
-    _db: None = database,
-    _merchants: list[Merchant] = three_merchants,
-) -> None:
-    merchants = await list_merchants()
+async def _(plan: Plan = plan) -> None:
+    await merchant_factory(plan=plan)
+    await merchant_factory(plan=plan)
+    await merchant_factory(plan=plan)
+
+    merchants = await list_merchants(plan.pk)
     assert len(merchants) == 3
 
 
 @test("listing merchants does not include deleted records")
-async def _(
-    _db: None = database,
-) -> None:
-    await merchant_factory()
-    await merchant_factory(status=ResourceStatus.DELETED)
-    await merchant_factory()
+async def _(plan: Plan = plan) -> None:
+    await merchant_factory(plan=plan)
+    await merchant_factory(plan=plan, status=ResourceStatus.DELETED)
+    await merchant_factory(plan=plan)
 
-    merchants = await list_merchants()
+    merchants = await list_merchants(plan.pk)
     assert len(merchants) == 2
 
 
+@test("can't list merchants on a non-existent plan")
+async def _(_db: None = database) -> None:
+    with raises(NoSuchRecord):
+        await list_merchants(uuid4())
+
+
 @test("can create a merchant")
-async def _(_db: None = database, plan: Plan = plan) -> None:
+async def _(plan: Plan = plan) -> None:
     merchant = await create_merchant({"name": "test"}, plan=plan)
     merchant = await get_merchant(merchant.pk, plan_ref=plan.pk)
     assert merchant.name == "test"
 
 
 @test("can update a merchant")
-async def _(_db: None = database, merchant: Merchant = merchant) -> None:
+async def _(merchant: Merchant = merchant) -> None:
     await update_merchant(merchant.pk, {"name": "updated"}, plan_ref=merchant.plan)
     merchant = await get_merchant(merchant.pk, plan_ref=merchant.plan)
     assert merchant.name == "updated"
 
 
 @test("updating a merchant that doesn't exist raises NoSuchRecord")
-async def _(_db: None = database, plan: Plan = plan) -> None:
+async def _(plan: Plan = plan) -> None:
     with raises(NoSuchRecord):
         await update_merchant(uuid4(), {"name": "updated"}, plan_ref=plan.pk)
 
 
 @test("updating a merchant on a plan that doesn't exist raises NoSuchRecord")
-async def _(_db: None = database, merchant: Merchant = merchant) -> None:
+async def _(merchant: Merchant = merchant) -> None:
     with raises(NoSuchRecord):
         await update_merchant(merchant.pk, {"name": "updated"}, plan_ref=uuid4())
 
 
 @test("updating a merchant on a plan it doesn't belong to raises NoSuchRecord")
-async def _(
-    _db: None = database, plan: Plan = plan, merchant: Merchant = merchant
-) -> None:
+async def _(plan: Plan = plan, merchant: Merchant = merchant) -> None:
     with raises(NoSuchRecord):
         await update_merchant(merchant.pk, {"name": "updated"}, plan_ref=plan.pk)
 
 
 @test("can delete a merchant")
-async def _(_db: None = database, merchant: Merchant = merchant) -> None:
+async def _(merchant: Merchant = merchant) -> None:
     await delete_merchant(merchant.pk, plan_ref=merchant.plan)
     with raises(NoSuchRecord):
         await get_merchant(merchant.pk, plan_ref=merchant.plan)
 
 
 @test("deleting a merchant that doesn't exist raises NoSuchRecord")
-async def _(_db: None = database, plan: Plan = plan) -> None:
+async def _(plan: Plan = plan) -> None:
     with raises(NoSuchRecord):
         await delete_merchant(uuid4(), plan_ref=plan.pk)

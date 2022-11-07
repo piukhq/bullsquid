3ecd66076e0b7532041eefe170a79107aeb7a3de
Bilal Leadbitter <bleadbitter@bink.com>
2022-11-07 16:36:37 +0000

    feat: :sparkles: Action log database table

---
+++ bullsquid/merchant_data/action_log/tables.py
@@ -0,0 +1,17 @@
+"""Action log table definitions."""
+from piccolo.columns import (
+    Text,
+    Timestamptz,
+)
+
+from bullsquid.merchant_data.tables import TableWithPK
+
+class Action(TableWithPK):
+    """Represents a logged action made by a user"""
+    
+    date = Timestamptz()
+    user = Text(required=True)
+    action = Text(required=True)
+    entity_changed = Text(required=True)
+    before_change = Text(required=True)
+    after_change = Text(required=True)

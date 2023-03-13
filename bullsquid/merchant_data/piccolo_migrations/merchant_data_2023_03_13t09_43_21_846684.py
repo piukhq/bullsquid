08c01bcb09d37b4fc946e453cce1e77e2ed86eb2
backwardspy <backwardspy@pigeon.life>
2023-03-13 09:49:11 +0000

    fix: add migration for created date field on base table

---
+++ bullsquid/merchant_data/piccolo_migrations/merchant_data_2023_03_13t09_43_21_846684.py
@@ -0,0 +1,136 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Text, Timestamptz
+from piccolo.columns.defaults.timestamptz import TimestamptzNow
+from piccolo.columns.indexes import IndexMethod
+
+ID = "2023-03-13T09:43:21:846684"
+VERSION = "0.105.0"
+DESCRIPTION = "add created date to base table"
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="merchant_data", description=DESCRIPTION
+    )
+
+    manager.add_column(
+        table_class_name="Location",
+        tablename="location",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="PSIMI",
+        tablename="psimi",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Merchant",
+        tablename="merchant",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Plan",
+        tablename="plan",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="PrimaryMID",
+        tablename="primary_mid",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="SecondaryMID",
+        tablename="secondary_mid",
+        column_name="created",
+        db_column_name="created",
+        column_class_name="Timestamptz",
+        column_class=Timestamptz,
+        params={
+            "default": TimestamptzNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    return manager

c1f2446c3ac77c98ad6c0edcf24f3d3d054baf4e
backwardspy <cl@bink.com>
2024-03-18 15:18:49 +0000

    fix(db): make Location.location_id required (#209)

---
+++ bullsquid/merchant_data/piccolo_migrations/merchant_data_2024_03_14t13_53_18_734009.py
@@ -0,0 +1,38 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Text
+from piccolo.table import Table
+
+ID = "2024-03-14T13:53:18:734009"
+VERSION = "0.121.0"
+DESCRIPTION = "make location ID required, and replace empty location IDs with UUIDs."
+
+
+class Location(Table):
+    pass
+
+
+async def forwards() -> MigrationManager:
+    manager = MigrationManager(
+        migration_id=ID, app_name="merchant_data", description=DESCRIPTION
+    )
+
+    async def patch_empty_location_ids() -> None:
+        await Location.raw(
+            "UPDATE location SET location_id = uuid_generate_v4() WHERE location_id = '' or location_id is null"
+        )
+
+    manager.add_raw(patch_empty_location_ids)
+
+    manager.alter_column(
+        table_class_name="Location",
+        tablename="location",
+        column_name="location_id",
+        db_column_name="location_id",
+        params={"null": False},
+        old_params={"null": True},
+        column_class=Text,
+        old_column_class=Text,
+        schema=None,
+    )
+
+    return manager
